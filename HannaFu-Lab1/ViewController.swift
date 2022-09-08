565c665a063b964c19194efb5e00c358347c3441
Hanna Fu <fuh1836college@gmail.com>
2022-09-08 18:47:29 -0500

    Initial Commit

---
+++ HannaFu-Lab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HannaFu-Lab1
+//
+//  Created by Sproull Student on 9/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
